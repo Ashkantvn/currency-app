190b36ecd679fddb44130c50ba63666c300f39a8
ashkan <ashkantavangar7@gmail.com>
2022-04-26 22:57:18 +0430

    create currency app

---
+++ src/Home.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+        <div>
+            <div>
+                <form action="">
+                    <h1>Search currency</h1>
+                    <input type="search" name="" id="" />
+                </form>
+            </div>
+            <div>
+                <div></div>
+            </div>
+        </div>
+    );
+}
+
+export default Home;
