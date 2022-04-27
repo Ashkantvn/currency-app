c3bec80b92f7c5d5f4920bd7cb38717c8c0e1162
ashkan <ashkantavangar7@gmail.com>
2022-04-27 22:11:01 +0430

    responsive design

--- src/Home.jsx
+++ src/Home.jsx
@@ -2,25 +2,25 @@ import React from 'react';
 
 const Home = (props) => {
     return (
-        <ul className='list d-flex justify-content-center flex-wrap'>
-            <div className='m col-2'>
+        <ul className='list row'>
+            <div className='m col m-2'>
                 <h4>{props.name}</h4>    
             </div>
-            <div className='m col-2'>
+            <div className='m col m-2'>
                 <h4>{props.symbol}</h4>    
             </div>
-            <div className='m col-2'>
+            <div className='m col m-2'>
                 <img src={props.imgurl} alt={props.name} />
             </div>
-            <div className='m col-2'>
+            <div className='m col m-2'>
                 <h4>Current price : </h4>
                 $ {props.currentPrice}
             </div>
-            <div className='m col-2'>
+            <div className='m col m-2'>
                 <h4>Price change percentage(in 24h):</h4>
-                <div className={props.pricechangePercentage > 0 ?'green m col-2':'red m col-2'}>{props.pricechangePercentage}%</div> 
+                <div className={props.pricechangePercentage > 0 ?'green m col m-2':'red m col m-2'}>{props.pricechangePercentage}%</div> 
             </div>
-            <div className='m col-2'>
+            <div className='m col m-2'>
                 <h4>Market_cap:</h4>
                 ${props.marketCap}
             </div>
