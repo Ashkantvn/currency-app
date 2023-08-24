5e50cbc2d7aa50103b828263c5f7f3cd8dcb5942
Ashkan <ashkantavangar7@gamil.com>
2023-08-25 01:11:22 +0330

    Create scroll to top of the page button

--- src/app.js
+++ src/app.js
@@ -3,7 +3,7 @@ import Card from './parts/Card';
 import { useFetch } from './hooks/useFetch';
 import Header from './parts/Header';
 import { currencyAppContext } from './hooks/useCurrencyContext';
-
+import {AiOutlineArrowUp} from "react-icons/ai"
 
 const App = () => {
     const fetchData = useFetch();
@@ -16,12 +16,18 @@ const App = () => {
             <Card key={coin.id} coin={coin} />
         )
     })
+
+    ///scroll to the top of the page
+    const topArrowButtonClickHandler = ()=>{
+        window.scrollTo(0,0);
+    }
     return (
         <main>
             <Header />
             <div className='coin'>
                 {mappingfetchData}
             </div>
+            <button className='top-link' onClick={topArrowButtonClickHandler}><AiOutlineArrowUp/></button>
         </main>
     );
 }
