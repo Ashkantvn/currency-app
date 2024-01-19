19d591b7bd3cdfba0143df36d41ca37e2ce026c3
Ashkan <ashkantavangar7@gmail.com>
2024-01-19 14:44:40 +0330

    Update useFetch custom hook

--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -3,15 +3,19 @@ import axios from 'axios';
 
 export const useFetch = () => {
     const [Data, setData] = useState([]);
-    
+    const fetchData =async ()=>{
+        try {
+            const fetchData  = await axios.get("https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false");
+            setData(fetchData);
+        } catch (error) {
+            setData(error.message);
+        }
+    }
     ////////////////////get data
     useEffect(() => {
-        axios.get("https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false")
-        .then((res) =>{
-            setData(res.data)
-        })
-        .catch(error =>{console.log(error);})
-    }, []);
+        fetchData();
+        }, [])
+        
     return Data ;
 }
 
