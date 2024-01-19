bb1d77ca3096fc6407111d1e8145722e8be233c9
Ashkan <ashkantavangar7@gmail.com>
2024-01-19 14:46:40 +0330

    Add mapper function to handle errors and shows Data

--- src/parts/Main.jsx
+++ src/parts/Main.jsx
@@ -6,17 +6,21 @@ import Card from './Card';
 export default function Main() {
     const fetchData = useFetch();
     const { Search } = useContext(currencyAppContext);
-
     //mapping fetchData
-    const FilterSearch = fetchData.filter(coin => coin.name.toLowerCase().includes(Search.toLowerCase()))
-    const mappingfetchData = FilterSearch.map(coin => {
-        return (
-            <Card key={coin.id} coin={coin} />
-        )
-    })
+    const mapper = ()=>{
+        if( typeof fetchData === "string" || fetchData.length === 0){return fetchData;}
+        const FilterSearch = fetchData.data.filter(coin => coin.name.toLowerCase().includes(Search.toLowerCase()));
+        const mappingfetchData = FilterSearch.map(coin => {
+            return (
+                <Card key={coin.id} coin={coin} />
+            )
+        });
+        return mappingfetchData.length>0?mappingfetchData:"Nothing Found !"
+    }
+    
     return (
         <main className='coin'>
-            {mappingfetchData}
+            {mapper()}
         </main>
     )
 }
