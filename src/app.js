7acf3458da13e86b3464ea2d9cc4fbbcb54a3407
Ashkan <ashkantavangar7@gmail.com>
2024-01-23 20:09:12 +0330

    update app.js

--- src/app.js
+++ src/app.js
@@ -1,42 +1,34 @@
-import React, { useContext, useRef } from 'react';
-import Header from './parts/Header';
-import {AiOutlineArrowUp} from "react-icons/ai"
-import Main from './parts/Main';
-import { currencyAppContext } from './hooks/useCurrencyContext';
-import GlobalStyle from './style/styledComponents/globalStyle';
+import React, { useContext, useRef } from "react";
+import Header from "./parts/Header";
+import Main from "./parts/Main";
+import { currencyAppContext } from "./hooks/useCurrencyContext";
+import GlobalStyle from "./style/styledComponents/globalStyle";
+import Footer from "./parts/Footer";
 
 const App = () => {
-    const { setishidden , isNightMode} = useContext(currencyAppContext);
-    const timer = useRef(0);// settimeout functions ID
+  const { setishidden, isNightMode } = useContext(currencyAppContext);
+  const timer = useRef(0); // settimeout functions ID
 
-    ///scroll to the top of the page
-    const topArrowButtonClickHandler = ()=>{
-        window.scrollTo(0,0);
+  ///scroll event for body element
+  window.addEventListener("scroll", () => {
+    setishidden((ishidden) => (ishidden = true));
+    if (timer.current !== 0) {
+      /// will stop showing the header if client is scrolling
+      clearTimeout(timer.current);
     }
+    timer.current = setTimeout(() => {
+      setishidden((ishidden) => (ishidden = false));
+    }, 300);
+  });
 
-    ///scroll event for body element 
-    window.addEventListener("scroll",()=>{
-      setishidden((ishidden)=>ishidden=true);
-      if (timer.current!==0){/// will stop showing the header if client is scrolling
-          clearTimeout(timer.current);
-      }
-      timer.current = setTimeout(()=>{
-          setishidden((ishidden)=>ishidden=false);
-      },300);
-    })
-  
-    
-    
-    return (
-        
-        <div>
-            <GlobalStyle isNightMode={isNightMode}/>
-            <Header />
-            <Main/>
-            <button className='top-link' onClick={topArrowButtonClickHandler}><AiOutlineArrowUp/></button>
-        </div>
-    );
-    }
+  return (
+    <>
+      <GlobalStyle isNightMode={isNightMode} />
+      <Header />
+      <Main />
+      <Footer />
+    </>
+  );
+};
 
 export default App;
-
