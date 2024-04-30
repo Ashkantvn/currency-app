fe1f68cde6ff72fcb8b7ebc0aa14c8dafe404afb
Ashkan <ashkantavangar7@gmail.com>
2024-04-30 23:36:36 +0330

    Update fetch function ,use session storage, and useEffect

--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -1,21 +1,39 @@
-import {useEffect , useState} from 'react';
-import axios from 'axios';
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 export const useFetch = () => {
-    const [Data, setData] = useState([]);
-    const fetchData =async ()=>{
-        try {
-            const fetchData  = await axios.get("https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false");
-            setData(fetchData);
-        } catch (error) {
-            setData(error.message);
-        }
-    }
-    ////////////////////get data
-    useEffect(() => {
-        fetchData();
-        }, [])
-        
-    return Data ;
-}
+  const controller = new AbortController();
+  const [Data, setData] = useState("");
 
+
+  const fetchData = async () => {
+    const sessionData = sessionStorage.getItem("coinData");
+    if (!sessionData) {
+      try {
+        const fetchData = await axios.get(
+          "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false",
+          { signal: controller.signal }
+        );
+        setData(fetchData.data);
+        sessionStorage.setItem("coinData", JSON.stringify(fetchData.data));
+      } catch (error) {
+        setData(error.message);
+      }
+    }else{
+        setData(()=>JSON.parse(sessionData));
+    }
+  };
+
+
+  ////////////////////get data
+  useEffect(() => {
+    if (Data.length===0) {
+      fetchData();
+    }
+    return () => {
+      controller.abort();
+    };
+  });
+
+  return Data;
+};
